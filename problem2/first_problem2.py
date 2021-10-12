68558cfd372642b68fff61d8526e7cec8e915257
Tetrodotoxin <eggis@me.com>
2021-10-11 23:16:38 -0400

    first_problem2.py finishe and correct

---
+++ problem2/first_problem2.py
@@ -0,0 +1,30 @@
+#PROBLEM 1: Considering the terms in the Fibonacci sequence (starting with 1&2)
+# whose values do not exceed four million, find the sum of the even-valued terms.
+
+#SOLUTION LEVEL(simple): For each value made by the fibonacci generator, if it
+# is divisible by 2, add it to the running_total.
+
+#COMPLEXITY: O(?)
+
+def fib_gen(num1,num2):
+    fib = num1+num2
+    return fib
+
+running_total = 0
+limit = 4000000
+# Ininitialize fibonacci values
+fib = 2
+old_fib = 1
+
+while fib <= limit:
+
+    if fib%2 == 0: # Is fib even
+        print(fib)
+        running_total += fib
+
+    new_fib = fib_gen(fib,old_fib)
+    old_fib = fib
+    fib = new_fib
+
+print("The sum of all even numbers from the Fibonacci sequence not exceeding 4000000 is...")
+print(running_total)
