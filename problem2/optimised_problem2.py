8d87c0e62e2d4251909832a87a4870fb1b6ab320
Tetrodotoxin <eggis@me.com>
2021-10-13 01:34:57 -0400

    truly improved optimised solution by utilising the odd/even pattern of the fib sequence and only calculating every third element to add to sum

--- problem2/optimised_problem2.py
+++ problem2/optimised_problem2.py
@@ -4,30 +4,32 @@
 #SOLUTION LEVEL(optimised): The iterative Fibonacci generator of the first
 # version was replaced by a formulaic version derived from the difference
 # equations. This poses no real benefit as to find the even values in the series
-# we need to compute all values anyway (TODO AJE: IS THIS TRUE???) but it's
-# interesting nonetheless!
+# until you realise that the fibonnaci series by definition follows the pattern
+# [odd, odd, even, odd, odd, even] which means instead of operating on every
+# integer you need only sum every third.
 
-#COMPLEXITY: O(?) [Assuming the formulaic fib generator holds against rounding
-# errors (IT DOESNT AFTER 71 ITERATIONS, see bottom)] TODO AJE: revise
+#COMPLEXITY: O(1/3n) which roughly == O(n), sue me this was tricky [Assuming the
+# formulaic fib generator holds against rounding errors (IT DOESNT AFTER 71
+# ITERATIONS, see tests at the bottom) ¯\_(ツ)_/¯ ]
 
 import math
 
-def nth_fib(n):
+def nth_fib(n): # Formulaic calculation of nth fibonnaci number
     root_of_5 = math.sqrt(5)
     fib = round(((1/root_of_5)*(((1+root_of_5)/2)**(n+1)))\
                -((1/root_of_5)*(((1-root_of_5)/2)**(n+1))))
     return fib
 
-n = 1
+n = 2 # Start at n = 2 because we are required to start at where fib == 2 which
+      # is the third element.
 limit = 4000000
 running_total = 0
 fib = nth_fib(n)
 while fib <= limit:
 
-    if fib%2 == 0: # Is fib even
-        running_total += fib
-
-    n += 1
+    running_total += fib
+    n += 3 # Because of the pattern of odds and evens in the fibonacci sequence
+           # (SEE SOLUTION DESCRIPTION) every third element is even
     fib = nth_fib(n)
 
 print("The sum of all even numbers from the Fibonacci sequence not exceeding 4000000 is...")
@@ -49,18 +51,15 @@ def test_form_fib_gen():
     form_fib = 1
     while iter_fib == form_fib:
 
-        # iterative fibonacci generator
+        # Iterative fibonacci generator
         new_fib = fib_gen(iter_fib,old_fib)
         old_fib = iter_fib
         iter_fib = new_fib
 
-        # formulaic fibonacci generator
+        # Formulaic fibonacci generator
         form_fib = nth_fib(n)
+        print(form_fib)
         n += 1
-
-        # print n every 10000 iterations
-        if n % 10000 == 0:
-            print(n)
 
         print(n)
     print("DONEZO")
@@ -68,4 +67,4 @@ def test_form_fib_gen():
     print(iter_fib)
     print(form_fib)
 
-test_form_fib_gen()#
+test_form_fib_gen() #
